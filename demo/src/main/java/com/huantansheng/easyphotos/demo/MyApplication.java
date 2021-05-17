485a328b02ec57fd14d7cbed1efc586dc733f165
joker <fugui@axzo.cn>
2021-05-17 10:50:58 +0800

    gradle & 依赖库升级

--- demo/src/main/java/com/huantansheng/easyphotos/demo/MyApplication.java
+++ demo/src/main/java/com/huantansheng/easyphotos/demo/MyApplication.java
@@ -2,7 +2,6 @@ package com.huantansheng.easyphotos.demo;
 
 import android.app.Application;
 
-import com.squareup.leakcanary.LeakCanary;
 import com.tencent.bugly.crashreport.CrashReport;
 
 /**
@@ -14,12 +13,6 @@ public class MyApplication extends Application {
     @Override
     public void onCreate() {
         super.onCreate();
-        if (LeakCanary.isInAnalyzerProcess(this)) {
-            // This process is dedicated to LeakCanary for heap analysis.
-            // You should not init your app in this process.
-            return;
-        }
         CrashReport.initCrashReport(getApplicationContext(), "4c251b8f40", false);
-        LeakCanary.install(this);
     }
 }
